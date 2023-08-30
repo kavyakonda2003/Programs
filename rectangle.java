7956bd146f299505d692954ce7c6e0bae3e26c00
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-08-30 17:45:33 +0530

    Create rectangle.java

---
+++ rectangle.java
@@ -0,0 +1,16 @@
+// Update the _ to solve the problem
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    int length = 11;
+        int width = 13;
+        System.out.println(length * width);
+        System.out.println(2 * (length + width));
+        width = 15; 
+        System.out.println(length * width);
+        System.out.println(2 * (length + width));
+	}
+}
