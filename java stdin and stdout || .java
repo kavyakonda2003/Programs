44d69f4dde8657ce7483323d37a557c0b21ae8dc
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-08-30 22:12:19 +0530

    Create java stdin and stdout || .java

---
+++ java stdin and stdout || .java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i;
+        double d;
+        String s;
+        i=scan.nextInt();
+        d=scan.nextDouble();
+        scan.nextLine();
+        s=scan.nextLine();
+        
+
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
