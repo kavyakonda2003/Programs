76555abca58ba1805319a7864da214e0b324c084
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-08-30 18:28:21 +0530

    Create Intro.java

---
+++ Intro.java
@@ -0,0 +1,8 @@
+class Codechef
+{
+    public static void main (String[] args)
+    {
+         int Number = 19;  //Update the blanks with the value assigned to Number
+        System.out.print(Number); //Output the variable to the console
+    }
+}
