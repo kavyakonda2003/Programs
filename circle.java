03cbce0551e823178fe4cd6b13ab87f687404cdd
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-08-31 22:23:04 +0530

    Create circle.java

---
+++ circle.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		double pi = 3.14;
+		double radius = 8.9;
+		double area = pi * radius * radius;
+		System.out.println("The Area of the given Circle is " + area);
+	}
+}
