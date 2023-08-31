b3c1c1f73fd527930edc40a95d45999a7c368f7c
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-08-31 22:44:47 +0530

    Create example3.java

---
+++ example3.java
@@ -0,0 +1,17 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = -50;
+		int b = 40;
+		// update your code below this line
+
+		System.out.println(a+b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+		
+		
+		
+	}
+}
