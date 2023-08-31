b0095afc881073bdc3e66fdd7af7a06e9270f6cd
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-08-31 19:31:45 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        int length = 45;
+        int width = 76;
+        int area = length * width;
+        System.out.println("The Area of the given rectangle is " + area);
+	}
+}
