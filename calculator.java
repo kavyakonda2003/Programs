7af488fc263c34ba3614dfbab48d6a53d81df6c8
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-02 18:10:06 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int a = 35;
+		int b = 23;
+		System.out.println("Sum is: " + (a+b));
+		System.out.println("Difference is: " + (a-b));
+	}
+}
