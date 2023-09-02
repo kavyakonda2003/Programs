080fc2ce0fbcdfa8b636d7a95dfaf22cf11adcdb
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-02 18:19:51 +0530

    Create if.java

---
+++ if.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int a = 25;
+		int b = 20;
+		if (25 > 20){
+		    System.out.print("Coding is Fun!");
+		}
+	}
+}
