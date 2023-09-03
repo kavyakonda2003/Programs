b4be7465a798d179086dbc160513a21306af5b78
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-03 21:48:56 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,11 @@
+// Update the '_' below to solve the problem
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int[] num = {1, 2, 3, 4,5 };
+		System.out.print(num[2]);
+	}
+}
