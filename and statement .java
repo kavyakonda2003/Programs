f0da47f4ca2320254d6e51f4adfa1bd2f9b1e73c
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-03 21:34:45 +0530

    Create and statement .java

---
+++ and statement .java
@@ -0,0 +1,22 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = 15;
+        int b = 35;
+        
+        if (a % 7 == 0 && a % 5 == 0) {
+            System.out.println("The number is divisible by both 5 & 7");
+        }
+        else {
+            System.out.println("The number is not divisible by both 5 & 7");
+        }
+
+        if (b % 7 == 0 && b % 5 == 0) {
+            System.out.println("The number is divisible by both 5 & 7");
+        }
+        else {
+            System.out.println("The number is not divisible by both 5 & 7");
+        }
+	}
+}
