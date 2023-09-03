1c992ec31550fa0e23f81ce60b21281cba166bb0
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-03 21:55:21 +0530

    Create displaying .java

---
+++ displaying .java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday"};
+		// update your code below this line
+        System.out.println(week[2]);
+        System.out.println(week[3]);
+
+	}
+}
