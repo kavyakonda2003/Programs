48731c22e6533b2cff02fc6b00fd0bb10b6c3a4b
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-03 21:29:17 +0530

    Create condition if .java

---
+++ condition if .java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner read= new Scanner(System.in);
+
+        int a = read.nextInt();
+        int b = read.nextInt();
+        int c = read.nextInt();
+        if (a>=c) {
+            System.out.println("Bravo!");
+        } else {
+            System.out.println("Try Again");
+        }
+
+        a = read.nextInt();
+        b = read.nextInt();
+        c = read.nextInt();
+        if (a>=c) {
+            System.out.println("Bravo!");
+        } else {
+            System.out.println("Try Again");
+        }
+	}
+}
