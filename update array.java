332a942bc41c22a3ce5ab00db0c2a01d00315431
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-03 21:52:43 +0530

    Create update array.java

---
+++ update array.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		String[] mnts = {"Jan", "Feb", " Dec", "Apr"};
+		// update your code below this line
+        mnts[2] = "mar";
+        System.out.println(mnts[2]);
+
+	}
+}
