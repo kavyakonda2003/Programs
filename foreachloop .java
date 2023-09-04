00bebf6d7eb4baf4de180b34125344c637e97cdd
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-04 20:59:39 +0530

    Create foreachloop .java

---
+++ foreachloop .java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int[] num = {10, 20, 30, 40, 50, 60, 70, 80};
+		for(int i :num ){
+		    System.out.println(i);
+		}
+	}
+}
