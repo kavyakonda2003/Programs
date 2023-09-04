560036ab294d93f17395240ba5854df030c6ae2c
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-04 20:49:58 +0530

    Create break .java

---
+++ break .java
@@ -0,0 +1,14 @@
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		for (int i = 0; i <= 15; i++) {
+            if (i == 13) {
+                break;
+            }
+            System.out.println(i);
+        }
+	}
+}
