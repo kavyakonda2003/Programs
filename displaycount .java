ded5e97e0694fad494fcabba9037bb2616687743
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-04 21:12:26 +0530

    Create displaycount .java

---
+++ displaycount .java
@@ -0,0 +1,10 @@
+class Codechef {
+  public static void main(String[] args) {
+    for (int i = 0; i <= 5; i += 2) {
+      if (i == 4) {
+        break;
+      }
+      System.out.println("Java");
+    }
+  }
+}
