d475925bad22e2b4c817451d181d01fb8da6db6c
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-04 20:55:11 +0530

    Create loopingthrougharrays .java

---
+++ loopingthrougharrays .java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+        String[] x = {"C", "O", "D", "I", "N", "G"};
+        for(int i = 0; i < 6; i++){
+            System.out.println(i + " = " + x[i]);
+        }
+	}
+}
