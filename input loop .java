8abc245a1460702ee7d1a8011d2d81ed06c863ea
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-04 19:10:45 +0530

    Create input loop .java

---
+++ input loop .java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner read = new Scanner(System.in);
+		int num = read.nextInt();
+		int a = 0;
+		// Update your code below this line
+		while(a<10){
+		System.out.println(a);
+		a++;
+
+
+
+		}
+	}
+}
