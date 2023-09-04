6088a577bcaeb5d0f266f2459b54aabb3b748c6a
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-04 20:33:57 +0530

    Create for array .java

---
+++ for array .java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int n = read.nextInt();
+		for(int i = 0; i < n; i++){
+		    System.out.println("java");
+		}
+	}
+}
