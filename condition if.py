ed669de2d794a93474b0ff247228022223bc981f
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2023-09-10 14:58:07 +0530

    Create condition if.py

---
+++ condition if.py
@@ -0,0 +1,19 @@
+a, c = map(int, input().split())      # Will cover this syntax for accepting multiple inputs later
+
+# Update the code below this line to complete Task 1
+if a>=c:
+    print("Bravo!")
+else:
+    print("try again")
+
+
+
+
+
+a, c = map(int, input().split())     # Will cover this syntax for accepting multiple inputs later
+
+# Update the code below this line to complete Task 2
+if a>=c:
+    print("Bravo!")
+else:
+    print("Try again")
