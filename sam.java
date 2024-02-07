c668236db74fc8d167530d82bf42a62ba7010db9
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-02-07 15:54:45 +0530

    Create sam.java

---
+++ sam.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        System.out.println(n);
+        
+
+	}
+}
