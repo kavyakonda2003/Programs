de2d6ae8461a30df9e3d31411214e102e91e1a0b
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-02-07 20:37:29 +0530

    Create noodles.java

---
+++ noodles.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+		int x;
+		int y;
+		x=sc.nextInt();
+		y=sc.nextInt();
+		int customers=x*y;
+		System.out.println(customers);
+
+	}
+}
