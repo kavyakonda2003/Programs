10e88047311860c5a6b95f9de29864fcf83d2997
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-02-09 21:45:02 +0530

    Create sam6.java

---
+++ sam6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		// write your code here
+		
+	}
+}
