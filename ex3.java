b5ec73506684ae2505a3e60ce21b07ea8d413aa0
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-02-11 20:30:57 +0530

    Create ex3.java

---
+++ ex3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int rs=sc.nextInt();
+		int cs=sc.nextInt();
+		int c=7-rs-cs;
+		System.out.println(c);
+		
+		
+	}
+}
