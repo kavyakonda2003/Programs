ba6f332e714588f2f44dc450b7650a33411b965f
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-02-14 21:18:05 +0530

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		if(M>N)
+		{
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+		
+		
+
+	}
+}
