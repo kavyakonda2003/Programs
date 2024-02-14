eea7282f09104fc5ae622feff8de41c1e1009c1c
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-02-14 21:44:39 +0530

    Create dice.java

---
+++ dice.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int test=sc.nextInt();
+		for(int i=0;i<test;i++)
+		{
+		    int X=sc.nextInt();
+		    if(X==6)
+		    {
+		        System.out.println("yes");
+		    }
+		    else{
+		        System.out.println("no");
+		    }
+		}
+
+	}
+}
