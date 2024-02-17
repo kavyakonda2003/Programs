29fb9be7a08e17751422e58b0efbf2ece8ef6b17
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-02-17 19:54:00 +0530

    Create unattemptattempt.java

---
+++ unattemptattempt.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if(X==404)
+		{
+		    System.out.println("not found");
+		    
+		}
+		else{
+		    System.out.println("found");
+		}
+
+	}
+}
