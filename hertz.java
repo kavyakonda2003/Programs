667cc9adf638957fe64f647e3225a2546155bf4f
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-02-26 20:31:27 +0530

    Create hertz.java

---
+++ hertz.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    if(x>=67 && x<=45000)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		    
+		}
+		
+		
+
+	}
+}
