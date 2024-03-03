a71f5da78ff8142f8dcc18bc2fb112e40606552e
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-03 20:54:39 +0530

    Create larger.java

---
+++ larger.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if(x>y)
+		    {
+		        System.out.println(x);
+		    }
+		    else
+		    {
+		        System.out.println(y);
+		    }
+		}
+		
+
+	}
+}
