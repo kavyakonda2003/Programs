e7e9cbf22805b7a62ecbed4da0595a7dc14f6a7f
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-05 14:37:43 +0530

    Create battery.java

---
+++ battery.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int X=sc.nextInt();
+		    if(X>=80)
+		    {
+		        System.out.println("yes");
+		    }
+		    else
+		    {
+		        System.out.println("no");
+		    }
+		}
+
+	}
+}
