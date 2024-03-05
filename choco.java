5c3cd0bc7aecca626ff4e5dabccd557b6d3ddec1
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-05 14:18:29 +0530

    Create choco.java

---
+++ choco.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		    int x=sc.nextInt();
+		    if(x%2==0)
+		    {
+		    System.out.println("yes");
+		    }
+		    else{
+		        System.out.println("no");
+		    }
+		}
+
+	}
+}
