39daccba728845a57149045c54a6888df24fb200
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-09 20:27:40 +0530

    Create ageing.java

---
+++ ageing.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		    int x=sc.nextInt();
+		    System.out.println(x-10);
+		}
+		
+
+	}
+}
