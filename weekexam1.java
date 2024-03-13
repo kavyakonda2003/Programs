d2ef89017b7b0f17f77d87446e13012226075291
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-13 20:30:24 +0530

    Create weekexam1.java

---
+++ weekexam1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X<50)
+		{
+		    System.out.println("Z");
+		}
+		else if(Y<50&&X>=50)
+		{
+		    System.out.println("F");
+		}
+		else
+		{
+		    System.out.println("A");
+		}
+		}
+
+	}
+}
