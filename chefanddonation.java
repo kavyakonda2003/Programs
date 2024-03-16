a517e05d63cbe3425858c60b2e2a0a1babf08d3d
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-16 20:22:45 +0530

    Create chefanddonation.java

---
+++ chefanddonation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int X=sc.nextInt();
+		    int Y=sc.nextInt();
+		    System.out.println(Y-X);
+		}
+		
+
+	}
+}
