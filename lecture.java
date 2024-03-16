9a136983d55f57280f79bd823d0ad69de3928e10
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-16 20:37:17 +0530

    Create lecture.java

---
+++ lecture.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    if(x<=30)
+		    {
+		        System.out.println("no");
+		    }
+		    else
+		    {
+		        System.out.println("yes");
+		    }
+		}
+
+	}
+}
