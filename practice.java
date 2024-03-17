3b143d7bbe5a7e114e0f7706d2db0fbac204a396
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-17 20:04:26 +0530

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int K=sc.nextInt();
+		    int X=sc.nextInt();
+		    int y=7*K;
+		    System.out.println(y-X);
+		}
+		
+
+	}
+}
