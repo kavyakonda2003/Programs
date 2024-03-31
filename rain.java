381879f461e6d0c7c2c2863b9488c2a6acd66016
kavyakonda2003 <143516809+kavyakonda2003@users.noreply.github.com>
2024-03-31 11:47:27 +0530

    Create rain.java

---
+++ rain.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    if(x<3)
+		    {
+		        System.out.println("Light");
+		    }
+		    else if(x>=3 && x<7)
+		    {
+		        System.out.println("Moderate");
+		    }
+		    else
+		    {
+		        System.out.println("Heavy");
+		    }
+		}
+
+	}
+}
